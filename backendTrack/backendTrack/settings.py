b994b197de349cf39b4a0fcf2e19e48a4150037f
rahulnegi20 <rahulnegi2021@gmail.com>
2021-10-23 12:47:47 +0530

    Done with error

--- backendTrack/backendTrack/settings.py
+++ backendTrack/backendTrack/settings.py
@@ -15,10 +15,10 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv() 
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -133,10 +133,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print('OJOSADADAM', BASE_DIR)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+django_heroku.settings(locals())
--- backendTrack/backendTrack/urls.py
+++ backendTrack/backendTrack/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-]
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
