88d7fd4c841bbe1e054015fc4cc04aa2c58b891c
rahulnegi20 <rahulnegi2021@gmail.com>
2021-10-24 13:57:41 +0530

    some minor change in views

--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
@@ -15,6 +16,18 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user"""
