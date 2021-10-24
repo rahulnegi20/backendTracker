a33271cf169f6f2bd89b2d882f635c862b9c84a1
rahulnegi20 <rahulnegi2021@gmail.com>
2021-10-24 14:00:02 +0530

    Now token url is returning the name too

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-# from rest_framework.settings import api_settings
+from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 
@@ -15,16 +15,16 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
 
     serializer_class = AuthTokenSerializer
-    # renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-        return ({
+        return Response({
             'token': token.key,
-            'user_id': user.pk,
+            'user_id': user.name,
             'email': user.email
         })
 
